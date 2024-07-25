0f96c7bde6307480270175f792020d4de0aba719
volkinano <147515210+imthematrixl@users.noreply.github.com>
2024-07-25 16:31:15 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let age;
+age = window.prompt(`what's your age?`)
+if (age > 18){
+    console.log("u can have access");
+   age = Number.age;
+}
+
+else{
+    console.log("u must be 18+ to enter the site");
+    age = Number.age;
+}
+
